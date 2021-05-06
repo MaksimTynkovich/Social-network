c6be7f6716e0aafe5cacab8a3a4c1900f9611520
MaxTynkovich <m_wildfires@mail.ru>
2021-05-06 22:35:57 +0300

    Refactoring

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -39,7 +42,7 @@ let store = {
         this._callSubscriber = observer
     },
     dispatch(action) { // { type: 'ADD-POST' }
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -48,7 +51,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = ''
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT'){
+        } else if (action.type === UPDATE_NEW_POST_TEXT){
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -61,7 +64,7 @@ export const addPostActionCreator = () => {
     }
   }
   
-export const updateNewPostTextActionCreator = () => {
+export const updateNewPostTextActionCreator = (text) => {
     return {type: 'UPDATE-NEW-POST-TEXT', newText: text }
   }
   
