5c78a6c740f32f91676019e641734e28b7246d52
MaxTynkovich <m_wildfires@mail.ru>
2021-05-06 22:12:47 +0300

    Create a function updateNewPostTextActionCreator

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -8,18 +8,23 @@ let addPostActionCreator = () => {
   }
 }
 
+let updateNewPostTextActionCreator = () => {
+  return {type: 'UPDATE-NEW-POST-TEXT', newText: text }
+}
+
+
 const MyPosts = (props) => {
   let postElements = props.posts.map(p => <Post message={p.message} likes={p.likes} />)
 
   let newPostElement = React.createRef()
 
   let addPost = () => {
-    props.dispatch({ type: 'ADD-POST'});
+    props.dispatch(addPostActionCreator());
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch({ type: 'UPDATE-NEW-POST-TEXT', newText: text});
+    let action = updateNewPostTextActionCreator(text);
     props.dispatch(action);
   }
 
