1ab9dc7943f50845c2c29e2fcd37b151f6b95fb3
MaxTynkovich <m_wildfires@mail.ru>
2021-05-09 23:40:35 +0300

    Refactoring

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,4 +1,23 @@
-const dialogsReducer = (state, action) => {
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY';
+const SEND_MESSAGE = 'SEND_MESSAGE';
 
-    return state;
+const dialogsReducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY:
+            state.newMessageBody = action.body;
+            return state;
+        case SEND_MESSAGE:
+            let body = state.newMessageBody;
+            state.newMessageBody = '';
+            state.messages.push({ id: 6, message: body })
+            return state;
+        default: state
+    }
 }
+
+export const sendMessageCreator = () => ({type: 'SEND_MESSAGE'})
+export const updateNewMessageBodyCreator = (body) => 
+    ({type: 'UPDATE_NEW_MESSAGE_BODY', body: body })
+      
+
+export default dialogsReducer
