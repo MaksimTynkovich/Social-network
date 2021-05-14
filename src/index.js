52340ee85c29b82b0281fe05d8f65f467d0e527e
MaxTynkovich <m_wildfires@mail.ru>
2021-05-14 20:59:57 +0300

    Bug fix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(store.getState());
 
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+});
 
 
 // API
