ca2b210b72c9b044bbedd382380a5ecd458deb62
MaxTynkovich <m_wildfires@mail.ru>
2021-05-16 15:06:28 +0300

    Add dependency from store

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import s from './MyPosts.module.css';
-import Post from './Post/Post';
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from './MyPosts';
 
@@ -8,16 +6,16 @@ import MyPosts from './MyPosts';
 
 const MyPostsContainer = (props) => {
     let addPost = () => {
-        props.dispatch(addPostActionCreator())
+        props.store.dispatch(addPostActionCreator())
     }
 
     let onPostChange = (text) => {
         let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.store.dispatch(action);
     }
 
     return (
-       <MyPosts updateNewPostText={onPostChange} addPost={addPost}/>
+       <MyPosts updateNewPostText={onPostChange} addPost={addPost} post={props.posts} />
     )
 }
 
