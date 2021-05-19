6e88219d2073f04e5da676b516cbc13e457d8cb2
MaxTynkovich <m_wildfires@mail.ru>
2021-05-19 22:32:30 +0300

    Fix My posts

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { addPostActionCreator, updateNewPostTextActionCreator } from "../../../redux/profile-reducer";
-import StoreContext from '../../../StoreContext';
-import MyPosts from './MyPosts';
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+import MyPosts from "./MyPosts";
+import StoreContext from "../../../StoreContext";
 
-const MyPostsContainer = (props) => {
-
+const MyPostsContainer = () => {
     return (
-        <StoreContext.Consumer> 
+        <StoreContext.Consumer>
             {
             (store) => {
                 let state = store.getState();
                 let addPost = () => {
-                    store.dispatch(addPostActionCreator())
+                    store.dispatch(addPostActionCreator());
                 }
-            
+
                 let onPostChange = (text) => {
                     let action = updateNewPostTextActionCreator(text);
                     store.dispatch(action);
                 }
-                <MyPosts updateNewPostText={onPostChange} addPost={addPost} posts={state.profilePage.posts} newPostText={state.profilePage.newPostText} />
+
+                return <MyPosts updateNewPostText={onPostChange}
+                                addPost={addPost}
+                                posts={state.profilePage.posts}
+                                newPostText={state.profilePage.newPostText}/>
             }
         }
         </StoreContext.Consumer>
